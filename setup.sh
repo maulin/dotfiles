c647b29c37daa8e704a04f98a1e97ec626ef8d8d
Maulin Pathare <maulin.pathare@shopify.com>
2022-01-13 10:11:18 -0500

    nvim updates

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ SPIN ]; then
+  sudo apt purge neovim
+  sudo add-apt-repository -y ppa:neovim-ppa/stable
+  sudo apt update
+fi
+
 if [ ! -e ~/.vim/autoload/plug.vim ]; then
   echo "installing plug for vim packages"
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -8,7 +14,10 @@ fi
 
 cwd=`pwd`
 echo "creating symlinks"
-for file in vimrc tmux.conf zshrc aliasrc test_commands.sh; do
+for file in vimrc tmux.conf zshrc test_commands.sh gitignore_global; do
   echo " * $file"
   ln -sf $cwd/$file $HOME/.$file
 done
+
+mkdir -p $HOME/.conig/nvim
+ln -sf $cwd/init.vim $HOME/.config/nvim/init.vim
