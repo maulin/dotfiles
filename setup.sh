1fdc383583f13da34f02c1c3fc68c8dd1fd94dfb
Maulin Pathare <maulin.pathare@shopify.com>
2022-12-20 11:28:50 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,4 +23,8 @@ if [ ! -d "~/.oh-my-zsh/" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+if [ $SPIN ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
 nvim +PlugInstall +qall
