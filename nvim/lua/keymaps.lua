a873d6836e4803dbbee62925e96361b05761353a
Maulin <maulin@hey.com>
2024-10-30 23:32:00 -0400

    Update nvim config and setup

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,45 @@
+vim.keymap.set('n', '<leader>w', ':set invwrap<cr>')
+vim.keymap.set('n', '<leader>p', ':set invpaste<cr>')
+
+-- Move between splits
+
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+-- Move between tabs
+
+vim.keymap.set('n', '<S-l>',  'gt')
+vim.keymap.set('n', '<S-h>', 'gT')
+
+-- Resize window splits
+
+vim.keymap.set('n', '<Up>',    '3<C-w>+')
+vim.keymap.set('n', '<Down>',  '3<C-w>-')
+vim.keymap.set('n', '<Left>',  '3<C-w>>')
+vim.keymap.set('n', '<Right>', '3<C-w><')
+
+-- Telescope
+
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Telescope grep' })
+vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', telescope.oldfiles, { desc = 'Telescope history' })
+
+-- NerdTree
+
+vim.NERDTreeShowHidden = 1
+vim.keymap.set('n', '<C-g>', ':NERDTreeToggle<cr>')
+vim.keymap.set('n', '<C-f>', ':NERDTreeFind<cr>')
+
+vim.keymap.set('v', 's', ':sort!<cr>')
+vim.keymap.set('n', ',,', '<C-^>')
+
+-- set a mark and come back to it after J so the cursor doesn't move
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- move lines that are visually selected
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- setup.sh
+++ setup.sh
@@ -1,30 +1,14 @@
 #!/usr/bin/env bash
 
-if [ ! -e ~/.vim/autoload/plug.vim ]; then
-  echo "installing plug for vim packages"
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 cwd=`pwd`
 echo "creating symlinks"
-for file in vimrc tmux.conf zshrc test_commands.sh gitignore_global gitconfig; do
+for file in tmux.conf zshrc gitconfig; do
   echo " * $file"
   ln -sf $cwd/$file $HOME/.$file
 done
 
-mkdir -p $HOME/.config/nvim
-ln -sf $cwd/init.vim $HOME/.config/nvim/init.vim
+ln -sf $cwd/nvim $HOME/.config/
 
 if [ ! -d "~/.oh-my-zsh/" ]; then
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-
-if [ $SPIN ]; then
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-fi
-
-nvim +PlugInstall +qall
